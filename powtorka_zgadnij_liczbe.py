b5202be98ab169380677e9c2eb4a8b857be57c5c
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-04 09:12:53 +0100

    Update

--- powtorka_zgadnij_liczbe.py
+++ powtorka_zgadnij_liczbe.py
@@ -8,6 +8,7 @@ while zgadywanaLiczba != szukanaLiczba:
         print("Zgadłeś!")
     elif zgadywanaLiczba > szukanaLiczba:
         print("Liczba jest za duża")
+        print("Różnica to: ", zgadywanaLiczba - szukanaLiczba)
     elif zgadywanaLiczba < szukanaLiczba:
         print("Zgadywana liczba jest za mała")
 
